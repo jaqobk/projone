9aad43c344992f2017159840ea50c16ff750bbaf
jaqobk <ijycob@gmail.com>
2018-08-15 17:33:40 +0300

    more designs

--- projone.py
+++ projone.py
@@ -5,12 +5,16 @@ from flask_admin import Admin
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+#Admin VIEW Here
+admin = Admin(app, name='myapp', template_mode='bootstrap3')
+
 #index template
 @app.route('/')
 def index():
-    return '<h1>Here I am with my coding manenos</h1>'
-@app.route('/home')
+    return render_template('home.html')
+
 #home template
+@app.route('/home')
 def home():
 	return render_template('home.html')
 #login template
@@ -24,9 +28,14 @@ def contactus():
 	return render_template('contactus.html')
 #end contact template
 #test nav
-@app.route('/nav')
-def nav():
-	return render_template('nav.html')
+@app.route('/base')
+def base():
+	return render_template('base.html')
+#Start Footer
+@app.route('/footer')
+def footer():
+	return render_template('footer.html')
+#end footer
 
 
 if __name__ == '__main__':
